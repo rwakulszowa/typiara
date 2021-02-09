2d99716cfad892f2d46f46595082dca9373f9e07
rwakulszowa <rwakulszowa1@gmail.com>
2021-02-09 20:45:49 +0100

    Fafa test.
    Test checking inference of a purely generic application.
    In haskell terms: `f a = f a`.
    The function is important, because it shows the system's ability to
    deduce shape constraints, even if no external type data is provided.

--- test/Infer/ExpressionSpec.hs
+++ test/Infer/ExpressionSpec.hs
@@ -8,36 +8,40 @@ import Data.List.NonEmpty (NonEmpty(..))
 import Test.Hspec
 
 import Typiara.Infer.Application
-import Typiara.Infer.FT
 import Typiara.Infer.Expression
-import Typiara.Infer.TypeEnv
+import Typiara.Infer.FT
 import Typiara.Infer.SampleTyp
+import Typiara.Infer.TypeEnv
+
+-- | Fixed type variant to avoid ambiguous inference failures.
+singleton' :: FT SampleTyp Int -> TypeEnv SampleTyp Int
+singleton' = singleton
 
 spec :: Spec
 spec = do
   describe "inferExpression" $ do
     it "Num" $ do
-      let ts = [(ref "a", singleton (T Num) :: TypeEnv SampleTyp Char)]
+      let ts = [(ref "a", singleton' (T Num))]
       let expr = Expression {args = [], application = (ref "a" :| [])}
-      inferExpression ts expr `shouldBe` (Right (singleton (T Num)))
+      inferExpression ts expr `shouldBe` (Right (singleton' (T Num)))
     it "(Num -> Num) | a" $ do
       let ts =
             [ (ref "f", TypeEnv [(Root, F 0 0), (NotRoot 0, T Num)])
-            , (ref "a", singleton Nil)
+            , (ref "a", singleton' Nil)
             ]
       let expr = Expression {args = [], application = (ref "f" :| [ref "a"])}
-      inferExpression ts expr `shouldBe` (Right (singleton (T Num)))
+      inferExpression ts expr `shouldBe` (Right (singleton' (T Num)))
     it "a => (Num -> Num) | a" $ do
       let ts =
             [ (ref "f", TypeEnv [(Root, F 0 0), (NotRoot 0, T Num)])
-            , (arg "a", singleton Nil)
+            , (arg "a", singleton' Nil)
             ]
       let expr =
             Expression {args = [Arg "a"], application = (ref "f" :| [arg "a"])}
       inferExpression ts expr `shouldBe`
         (Right (TypeEnv [(Root, F 0 1), (NotRoot 0, T Num), (NotRoot 1, T Num)]))
     it "f => f | Num" $ do
-      let ts = [(arg "f", singleton Nil), (ref "a", singleton (T Num))]
+      let ts = [(arg "f", singleton' Nil), (ref "a", singleton' (T Num))]
       let expr =
             Expression {args = [Arg "f"], application = (arg "f" :| [ref "a"])}
       inferExpression ts expr `shouldBe`
@@ -48,4 +52,22 @@ spec = do
               , (NotRoot 1, Nil)
               , (NotRoot 2, T Num)
               , (NotRoot 3, Nil)
+              ]))
+    it "f a => f | a" $ do
+      let ts = [(arg "f", (singleton' Nil)), (arg "a", singleton' Nil)]
+      let expr =
+            Expression
+              {args = [Arg "f", Arg "a"], application = (arg "f" :| [arg "a"])}
+      inferExpression ts expr `shouldBe`
+        (Right
+           (TypeEnv
+              [ (Root, F 0 1)
+              , (NotRoot 0, F 2 3)
+              , (NotRoot 1, F 5 6)
+              , (NotRoot 2, Nil)
+              -- 4 missing to match the exact return value.
+              -- TODO: custom `Eq` implementation, ignoring id values.
+              , (NotRoot 3, Nil)
+              , (NotRoot 5, Nil)
+              , (NotRoot 6, Nil)
               ]))
